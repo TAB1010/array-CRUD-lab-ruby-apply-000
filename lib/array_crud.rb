de7c86466529cf00f527dcf40dc7fb64d965f7db
KHANDKER HASSAN <khassanasgar@gmail.com>
2020-05-31 18:45:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,14 @@ end
 def retrieve_last_element_from_array(array)
 
 end
-def create_an_empty_array
-  []
+def find_element_index(array, value_to_find)
+  # Add your solution here
 end
+
+def find_max_value(array)
+  # Add your solution here
+end
+
+def find_min_value(array)
+  # Add your solution here
 end
